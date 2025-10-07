6eac93f226fccf0db03c21351d2a98d3298d8d6f
Simon Kiss <sjkiss@gmail.com>
2025-10-07 11:09:36 -0400

    update in master script

--- 1_housing_class_master.R
+++ 1_housing_class_master.R
@@ -18,8 +18,9 @@ library(crosstable)
 library(labelled)
 library(modelsummary)
 library(nnet)
+library(car)
 #Make a vector of common variables
-common_vars<-c("vote", "occupation_oesch_6", "own_rent", "sub_class", "election", "prov", "quebec")
+common_vars<-c("vote", "age", "occupation_oesch_6", "own_rent", "sub_class", "election", "prov", "quebec")
 #bind the two together
 ces25b %>%
   bind_rows(., ces84)->ces
@@ -30,11 +31,17 @@ ces %>%
 #Conduct recodes
 ces$vote<-factor(as_factor(ces$vote), levels=c("Conservative", "Liberal", "NDP", "Bloc", "Green", "Other"))
 ces$vote2<-factor(ces$vote, levels=c("Conservative", "Liberal", "NDP", "Bloc"))
+ces$age<-as.numeric(ces$age)
+ces$sub_class2<-car::Recode(ces$sub_class, "'Lower Class'='Working Class';
+       'Upper-Middle Class'='Upper Class'", levels=c("Middle Class", "Working Class",
+                                                     "Upper Class"))
+ces$own_rent
+ces$own_rent<-Recode(ces$own_rent, "'Other'='Rent'", levels=c("Rent", "Own"))
+ces$election<-as.factor(ces$election)
 lookfor(ces84, "belongs")
 
 
-#Model Vote by occupation class by election
-
+#### Model Vote by occupation class by election
 #Nest the two elections
 ces %>%
   nest(-election) %>%
@@ -53,26 +60,30 @@ modelsummary(models$model[[2]], shape=model+term~response,
              output="model1_1984.html", title="1984")
 
 
-#Distribution of Subjective Class by election
+#### Distribution of Subjective Class by election
 ces %>%
-  group_by(election, sub_class) %>%
+  group_by(election, sub_class2) %>%
   summarize(n=n()) %>%
-  filter(!is.na(sub_class)) %>%
+  filter(!is.na(sub_class2)) %>%
   mutate(pct=n/sum(n)) %>%
-  arrange(sub_class) %>%
-  ggplot(., aes(y=sub_class, fill=factor(election, levels=c("1984", "2025")), x=pct))+
+  arrange(sub_class2) %>%
+  ggplot(., aes(y=fct_relevel(sub_class2, "Working Class"), fill=factor(election, levels=c("1984", "2025")), x=pct))+
   geom_col(position=position_dodge2(reverse = TRUE))+
   theme_minimal()+
   scale_fill_manual(values=c("black", "lightgrey"))+
   labs(fill="Election", x="Percent")
 ggsave("plots/figure_1_change_sub_class.png")
 
+# Check chisq
 chisq.test(table(ces$election, ces$sub_class))
 
-# Model subjective class on time
+#### Model subjective class on time
 mod_time<-multinom(sub_class~as.factor(election), data=ces)
-modelsummary(mod_time, stars=T, shape=model+term~response, output="model_time_subjective_class.html", fmt=2)
-# Check Class by subjective social class
+modelsummary(mod_time, stars=T,
+             shape=model+term~response,
+             output="model_time_subjective_class.html", fmt=2)
+
+#### Check Class by subjective social class
 
 ces %>%
   group_by(election) %>%
@@ -90,28 +101,10 @@ ces %>%
   theme(legend.position="bottom")
   ggsave(filename="plots/figure_2_effect_of_class_on_subjective_social_class.png", width=10, height=5)
 
-#I think we need to change the contrasts of these categorical variables
-# To make this interpretable.
-# The standard contrast is to copmpare each categorical level with a reference
-  # Category
-  # e.g. to compare the effect of being a skilled worker with that of being an unskilled worker
-  # That makes sense, but does it make the same sense to compare the effect of being a professional
-  # on picking a subjective social class compared to being an unskilled worker?
-
-# To me, because these are effectively ordered categorical variables (low class to high class)
-  # it makes more sense to contrast each level with the average of all levels below it.
-  # these are known as helmert contrasts.
-  # So the idea would be to compare the effect of being a "skilled worker" with being an "unskilled worker"
-  # but then to compare the effect of being a semi-professional associate manager with the average of the two lower categories, e.g. Skilled and unskilled
-  # and then to keep the chain going up to managers.
-# I implement this gbelow.
-  #Set contrasts for both categorical variables
-  # but I am *really* stretching here!
-
-contrasts(ces$occupation_oesch_6)<-contr.helmert(6)
-contrasts(ces$sub_class)<-contr.helmert(5)
-#Fit model
-mod_occupation<-multinom(sub_class~occupation_oesch_6*as.factor(election), data=ces)
+#### Model subjective class on objective class for each election
+# Use three category subjective class for simplicity
+mod_occupation<-multinom(sub_class2~occupation_oesch_6*as.factor(election), data=ces)
+summary(mod_occupation)
 modelsummary(mod_occupation, shape=model+term~response, stars=T,
              coef_rename=c("occupation_oesch_61"="Skilled versus all below",
                            "occupation_oesch_62"="Semi-Professionals Associates versus all below",
@@ -121,17 +114,78 @@ modelsummary(mod_occupation, shape=model+term~response, stars=T,
                            "as.factor(election)2025"="2025"),
              output="subjective_class_on_oesch_by_election.html")
 
-dat<-expand.grid(
-  occupation_oesch_6=c("Professionals","Managers"),
-  election=factor(c(1984, 2025)),
-  sub_class=c("Lower Class", "Working Class", "Middle Class",
-              "Upper-Middle Class", "Upper Class")
-)
+library(marginaleffects)
 
-slopes(mod_occupation, newdata=dat,
-            by=c("occupation_oesch_6", "sub_class")) %>% view()
-  ggplot(., aes(y=occupation_oesch_6, x=estimate,col=group))+
-  geom_point()+facet_grid(~election)
+avg_predictions(mod_occupation, variables="election", by=c("occupation_oesch_6", "election")) %>%
+  ggplot(., aes(y=occupation_oesch_6, x=estimate, col=election))+
+  geom_pointrange(aes(xmin=conf.low, xmax=conf.high), position=position_dodge(width=0.25))+
+  facet_grid(~fct_relevel(group, "Working Class"))
+ggsave(here("plots/figure3_effect_class_time.png"), width=8, height=4)
+
+#### Integrate Home Ownership
+# Probability of owning a home by age
+# Fit a model checking the effet of age (life cycle) on ownership
+# We use a direct term for age and a quadratic that permits the model to have a u-shaped vurve
+# And we interact each with time.
+mod_own<-glm(own_rent~age*as.factor(election)+I(age^2)*as.factor(election), data=ces, family="binomial")
+modelsummary(mod_own, stars=T)
+# visualize the effects
+avg_predictions(mod_own,
+                newdata=datagrid(age=seq(20,80, by=2),
+                                 election=factor(c("1984", "2025"))),
+                by=c( "age", "election")) %>%
+  ggplot(., aes(x=age, y=estimate, col=election))+
+  geom_smooth(method="loess")+geom_ribbon(aes(ymin=conf.low, ymax=conf.high, fill=election), alpha=0.2, linewidth=0)+theme_minimal()+
+  theme(legend.position="bottom")+guides(fill='none')+labs(y="Probability", x="Age",
+                                       fill="", col="Election",
+                                       title="Probability of Reporting Owning A Home\n1984 and 2025 Canada Election Studies")
+library(here)
+ggsave(filename=here("plots/figure_4_p_homeownership_by_age.png"), width=8, height=6)
+
+# Now check to see if home owership changes the
+mod_occupation_own_1984<-multinom(sub_class2~own_rent*age*I(age^2)*occupation_oesch_6, data=subset(ces, election==1984))
+mod_occupation_own_2025<-multinom(sub_class2~own_rent*age*I(age^2)*occupation_oesch_6, data=subset(ces, election==2025))
+ces$occupation_oesch_6
+#Generate predicted probabilities of
+avg_predictions(mod_occupation_own_1984,
+                newdata=datagrid(age=seq(25,60, by=5),
+                                 occupation_oesch_6=factor(c("Professionals", "Skilled Workers")),
+                                 own_rent=factor(c("Rent", "Own"))),
+                by=c("age", "occupation_oesch_6", "own_rent")) %>%
+  data.frame() %>% mutate(Election=1984) ->preds_1984
+  avg_predictions(mod_occupation_own_2025,
+                  newdata=datagrid(age=seq(25,60, by=5),
+                                   occupation_oesch_6=factor(c("Professionals", "Skilled Workers")),
+                                   own_rent=factor(c("Rent", "Own"))),
+                  by=c("age", "occupation_oesch_6", "own_rent")) %>%
+    data.frame() %>% mutate(Election=2025)->preds_2025
+  preds_1984 %>% bind_rows(preds_2025) %>%
+  ggplot(., aes(x=age, y=estimate, col=occupation_oesch_6, linetype=own_rent))+
+  geom_smooth(method="loess", se=F)+facet_grid(Election~fct_relevel(group, "Working Class"))
+avg_predictions(mod_occupation_own_2025,
+                newdata=datagrid(age=seq(25,60, by=5),
+                                 occupation_oesch_6=factor(c("Professionals", "Skilled Workers")),
+                                 own_rent=factor(c("Rent", "Own"))),
+                by=c("age", "occupation_oesch_6", "own_rent")) %>%
+  ggplot(., aes(x=age, y=estimate, col=occupation_oesch_6, linetype=own_rent))+
+  geom_smooth(method="loess", se=F)+facet_grid(~fct_relevel(group, "Working Class"))
+avg_comparisons(mod_occupation_own_1984,
+  newdata=datagrid(
+    age=seq(25,65, by=10),
+    occupation_oesch_6=factor(c("Professionals", "Skilled Workers")),
+    own_rent=factor(c("Rent", "Own"))), variables=c("own_rent"), by=c("age", "occupation_oesch_6")) %>%
+  ggplot(., aes(x=age, y=estimate, col=occupation_oesch_6))+
+  geom_smooth(method="loess", span=2)+
+  facet_grid(~fct_relevel(group, "Working Class"))
+
+avg_comparisons(mod_occupation_own_2025,
+                newdata=datagrid(
+                  age=seq(25,65, by=10),
+                  occupation_oesch_6=factor(c("Professionals", "Skilled Workers")),
+                  own_rent=factor(c("Rent", "Own"))), variables=c("own_rent"), by=c("age", "occupation_oesch_6")) %>%
+  ggplot(., aes(x=age, y=estimate, col=occupation_oesch_6))+
+  geom_smooth(method="loess", span=2)+
+  facet_grid(~fct_relevel(group, "Working Class"))
 
 write.csv(ces,"housing_ces.csv")
 write_dta(ces, "housing_ces.dta")
