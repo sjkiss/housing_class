4037b771f4720ec6b5756c8f9d0cdd84a1e0a543
sjkiss <sjkiss@gmail.com>
2026-04-24 11:38:03 -0400

    UPdate afternoon Apr 24

--- R_Code/3_Analysis.R
+++ R_Code/3_Analysis.R
@@ -1,8 +1,12 @@
 source("1_housing_class_master.R")
 
-#Set theme
-theme_set(theme_minimal()+theme(legend.position="bottom"))
+####Set up code ###
+# Set theme for plots
+theme_set(theme_minimal(base_size=20)+theme(legend.position="bottom"))
 
+#Make vector for gof_omit
+# we always omit these GOF from model print-outs
+gof_omit_vector<-c("AIC|BIC|Log.Lik.|F")
 
 #### Model Vote by occupation class by election ####
 
@@ -64,12 +68,148 @@ model4_list %>%
   geom_vline(xintercept=0, linetype=2)
 
 
-#### Concordance
+#### Concordance ####
+# Basic concordance plot
+# Note: This plot can be modified by replacing Percent
+# with a count of respondents.
+#That will show the collapse of working class respondents in the 2025 sample
+# Do do that you need to just replace the x with n, rather than Percent
+ces %>%
+  #Count respondents by objective class and their concordance status
+  count(election, obj_class2,concordance) %>%
+  #Form groups of election and objective class to calcluate hwat percentage
+  # of objective classes are concordant, deflators or inflators
+  group_by(election, obj_class2) %>%
+#Filter out missing concordants
+  #These will be people who do not have an objective social class
+  # mostly because of poor occupation coding
+    filter(!is.na(concordance)) %>%
+  #Calculate percent
+  mutate(Percent=n/sum(n)) %>%
+    ggplot(., aes(x=Percent, y=fct_relevel(concordance, "Deflator", "Concordant"), fill=election))+
+  geom_col( position=position_dodge(reverse=T))+
+  labs(x="Percent", y="Objective Social Class", fill="Election", title="Cocnordance by Objective Social Class, 1984 and 2025")+
+  facet_grid(~fct_relevel(obj_class2,"Working Class"), scales="free_y")+
+  scale_fill_grey()+scale_x_continuous(labels = scales::label_percent())
+#Save plot
+ggsave(filename=here("plots/concordance_objective_class.png"), width=10, height=8)
 
-model1_concordance_vote_84_roc<-multinom(vote2~concordance, data=subset(ces,election==1984&quebec!=1))
-model1_concordance_vote_84_qc<-multinom(vote2~concordance, data=subset(ces,election==1984&quebec==1))
-model1_concordance_vote_25_roc<-multinom(vote2~concordance, data=subset(ces,election==2025&quebec!=1))
-model1_concordance_vote_25_qc<-multinom(vote2~concordance, data=subset(ces,election==2025&quebec==1))
+# Is Concordance Affected By Home ownership?
+# #Report Class concordance by material class
+
+# This is a bit tricky because we have this problem where
+# working class people cannot be deflators and
+# upper class people cannot be inflators
+# That will leave unused factor levels in the different data sets
+# Multinom() does not like that
+# Set up working class data frame
+
+ces_working_class <-filter(ces, obj_class2=="Working Class") %>%
+  droplevels()
+ces_upper_class <-filter(ces, obj_class2=="Upper Class") %>%
+  droplevels()
+# we don't need the droplevels() here because middle class can be
+# inflators or deflators
+ces_middle_class<-filter(ces, obj_class2=="Middle Class")
+
+controls<-c("income_tertile", "degree")
+controls
+mod_own_working_84<-glm(concordance~own_rent, data=subset(ces_working_class, election=="1984"), family="binomial")
+mod_own_working_84_controls<-update(mod_own_working_84,reformulate(c("own_rent+income_tertile", controls)))
+mod_own_working_25<-glm(concordance~own_rent, data=subset(ces_working_class, election=="2025"), family="binomial")
+mod_own_working_25_controls<-update(mod_own_working_25,reformulate(c("own_rent+income_tertile", controls)))
+mod_own_upper_84<-glm(concordance~own_rent, data=subset(ces_upper_class, election=="1984"), family="binomial")
+mod_own_upper_84_controls<-update(mod_own_upper_84,reformulate(c("own_rent+income_tertile", controls)))
+mod_own_upper_25<-glm(concordance~own_rent, data=subset(ces_upper_class, election=="2025"), family="binomial")
+mod_own_upper_25_controls<-update(mod_own_upper_25,reformulate(c("own_rent+income_tertile", controls)))
+mod_own_middle_84<-multinom(concordance~own_rent, data=subset(ces_middle_class,election=="1984"))
+mod_own_middle_84_controls<-update(mod_own_middle_84, reformulate(c("own_rent+income_tertile", controls)))
+mod_own_middle_25<-multinom(concordance~own_rent, data=subset(ces_middle_class,election=="2025"))
+mod_own_middle_25_controls<-update(mod_own_middle_25, reformulate(c("own_rent+income_tertile", controls)))
+
+mod_own_list<-list("Working Classs\n1984 No controls"=mod_own_working_84,
+                   "Working Classs\n1984 Controls"=mod_own_working_84_controls,
+                   "Working Classs\n2025 No Controls"=mod_own_working_25,
+                   "Working Classs\n2025 Controls"=mod_own_working_25_controls,
+                   "Upper Classs\n1984 No Controls"=mod_own_upper_84,
+                   "Upper Classs\n1984 Controls"=mod_own_upper_84_controls,
+                   "Upper Class\n2025 No Controls"=mod_own_upper_25,
+                   "Upper Class\n2025 Controls"=mod_own_upper_25_controls
+                   )
+modelsummary(mod_own_list, stars=T, coef_rename = c("own_rentOwn"="Ownership"), gof_omit=gof_omit_vector, title="Effect of Ownership on Class Concordance, 1984 and 2025, Working and Upper Class", fmt=2)
+# Make the list of of middle class ownership models
+mod_own_middle_list<-list("Middle Class 1984 No Controls"=mod_own_middle_84,
+                          "Middle Class 1984 Controls"=mod_own_middle_84_controls,
+                          "Middle Class 2025 No Controls"=mod_own_middle_25,
+                          "Middle Class 2025 Controls"=mod_own_middle_25_controls
+                          )
+#Print
+modelsummary(mod_own_middle_list, stars=T, shape=term~model+response,
+             coef_rename=c("own_rentOwn"="Ownership"), gof_omit=gof_omit_vector,
+             title="Effect of Ownership on Class Concordance 1984 and 2025, Middle Class", fmt=2)
+
+# Generate an effect plot for middle class only
+#
+library(marginaleffects)
+summary(mod_own_middle_84)
+#Let's show the effect plot with the controls
+# You have to go back up to the list of models for middle class
+# And find the ones that have the controls
+# If you wanted to show the ones without the controls
+# You would switch these numbers
+mod_own_middle_list[c(2,4)]%>%
+  map(., avg_comparisons, variables=c("own_rent"), type="probs") %>%
+  list_rbind() %>%
+  mutate(Year=c(rep(1984, 3), rep(2025, 3))) %>%
+  ggplot(., aes(x=fct_relevel(group, "Deflator"), y=estimate))+
+  geom_pointrange(aes(ymin=conf.low, ymax=conf.high))+
+    facet_grid(~Year)+
+  labs(x="Concordance", y="Delta Probability",
+       title="Effect of home ownership on concordance, controlling for income and education\n1984 and 2025")
+
+#### Can we link this to vote at all?
+# What is the research question?
+# Is there a relationship between concordance and vote?
+mod1_vote_concordance_working_qc_84<-multinom(vote2~concordance, data=subset(ces_working_class,election==1984&quebec!=1))
+mod1_vote_concordance_working_roc_84<-multinom(vote2~concordance, data=subset(ces_working_class,election==1984&quebec==1))
+mod1_vote_concordance_working_qc_25<-multinom(vote2~concordance, data=subset(ces_working_class,election==2025&quebec!=1))
+mod1_vote_concordance_working_roc_25<-multinom(vote2~concordance, data=subset(ces_working_class,election==2025&quebec==1))
+
+mod1_vote_concordance_upper_qc_84<-multinom(vote2~concordance, data=subset(ces_upper_class,election==1984&quebec!=1))
+mod1_vote_concordance_upper_roc_84<-multinom(vote2~concordance, data=subset(ces_upper_class,election==1984&quebec==1))
+mod1_vote_concordance_upper_qc_25<-multinom(vote2~concordance, data=subset(ces_upper_class,election==2025&quebec!=1))
+mod1_vote_concordance_upper_roc_25<-multinom(vote2~concordance, data=subset(ces_upper_class,election==2025&quebec==1))
+
+mod1_vote_concordance_middle_qc_84<-multinom(vote2~concordance, data=subset(ces_middle_class,election==1984&quebec!=1))
+mod1_vote_concordance_middle_roc_84<-multinom(vote2~concordance, data=subset(ces_middle_class,election==1984&quebec==1))
+mod1_vote_concordance_middle_qc_25<-multinom(vote2~concordance, data=subset(ces_middle_class,election==2025&quebec!=1))
+mod1_vote_concordance_middle_roc_25<-multinom(vote2~concordance, data=subset(ces_middle_class,election==2025&quebec==1))
+
+# model1_concordance_vote_25_roc<-multinom(vote2~concordance, data=subset(ces,election==2025&quebec!=1))
+# model1_concordance_vote_25_qc<-multinom(vote2~concordance, data=subset(ces,election==2025&quebec==1))
+vote_concordance_working_list<-list("QC 1984"=mod1_vote_concordance_working_qc_84,
+                            "QC 2025"=mod1_vote_concordance_working_qc_25,
+                            "ROC 1984"=mod1_vote_concordance_working_roc_84,
+                            "ROC 2025"=mod1_vote_concordance_working_roc_25
+                            )
+names(vote_concordance_working_list)
+vote_concordance_middle_list<-list("QC 1984"=mod1_vote_concordance_middle_qc_84,
+                                    "QC 2025"=mod1_vote_concordance_middle_qc_25,
+                                    "ROC 1984"=mod1_vote_concordance_middle_roc_84,
+                                    "ROC 2025"=mod1_vote_concordance_middle_roc_25
+)
+vote_concordance_upper_list<-list("QC 1984"=mod1_vote_concordance_upper_qc_84,
+                                   "QC 2025"=mod1_vote_concordance_upper_qc_25,
+                                   "ROC 1984"=mod1_vote_concordance_upper_roc_84,
+                                   "ROC 2025"=mod1_vote_concordance_upper_roc_25
+)
+names(vote_concordance_middle_list)
+modelsummary(vote_concordance_working_list, fmt=2, stars=T,shape=term~response+model,
+             gof_omit=gof_omit_vector)
+modelsummary(vote_concordance_middle_list, fmt=2, stars=T,shape=term~response+model,
+             gof_omit=gof_omit_vector)
+modelsummary(vote_concordance_middle_list, fmt=2, stars=T,shape=term~response+model,
+             gof_omit=gof_omit_vector)
 #these models break out the material classes to see how class concordance interacts with vote ces$occupation_oesch_5
 model1a_concordance_vote_84_roc<-update(model1_concordance_vote_84_roc, .~., data=subset(ces,election==1984&quebec!=1&occupation_oesch_5=="Unskilled workers"))
 model1b_concordance_vote_84_roc<-update(model1_concordance_vote_84_roc, .~., data=subset(ces,election==1984&quebec!=1&occupation_oesch_5=="Skilled workers"))
@@ -83,19 +223,55 @@ model1d_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~., dat
 #model1c_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~., data=subset(ces,election==2025&quebec!=1&occupation_oesch_5=="Self-employed"))
 # model1d_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~., data=subset(ces,election==2025&quebec!=1&occupation_oesch_5=="Professionals"))
 # model1e_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~., data=subset(ces,election==2025&quebec!=1&occupation_oesch_5=="Managers"))
+
 #These models add home ownership to the mdoels
 #Take out concordance and add own_rent
+#1984
+model2_concordance_vote_84_roc<-update(model1_concordance_vote_84_roc, .~-concordance+own_rent)
+#re-add concordance
+model3_concordance_vote_84_roc<-update(model1_concordance_vote_84_roc, .~concordance+own_rent)
+#add interaction term
+model4_concordance_vote_84_roc<-update(model1_concordance_vote_84_roc, .~concordance*own_rent)
+#Take out concordance and add own_rent
+#2025
 model2_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~-concordance+own_rent)
-#replace
+#re-add concordance
 model3_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~concordance+own_rent)
 #add interaction term
 model4_concordance_vote_25_roc<-update(model1_concordance_vote_25_roc, .~concordance*own_rent)
 #summary(model4_concordance_vote_25_roc)
-#Report Class concordance over time 1984 and 2025
+#Report basic lass concordance over time 1984 and 2025
 concordance_vote_list<-list("1984"=model1_concordance_vote_84_roc, "2025"=model1_concordance_vote_25_roc)
 modelsummary(concordance_vote_list, stars=T,
-             fmt=2, shape=term~model+response)
-#Report Class concordance by material class
+             fmt=2, shape=term~model+response, gof_omit=gof_omit_vector)
+concordance_vote_own_list<-list("1984"=model3_concordance_vote_84_roc, "2025"=model3_concordance_vote_25_roc)
+modelsummary(concordance_vote_own_list, stars=T,
+             fmt=2, shape=term~model+response, gof_omit=gof_omit_vector)
+# Show concordance and homeownership interaction in 84 and 2025
+concordance_own_rent_list<-list("1984"=model4_concordance_vote_84_roc, "2025"=model4_concordance_vote_25_roc)
+modelsummary(
+  concordance_own_rent_list,
+  fmt=2,
+  stars=T, gof_omit=gof_omit_vector,
+  shape=term~model+response
+)
+ces$concordance
+
+ces_working_class <-filter(ces, obj_class2=="Working Class") %>%
+  droplevels()
+ces_upper_class <-filter(ces, obj_class2=="Upper Class") %>%
+  droplevels()
+model_concordance_vote_working_class<-multinom(vote2~concordance*own_rent,
+data=subset(ces_working_class, election==1984)
+
+model_concordance_vote_middle_class<-update(model_concordance_vote_working_class, data=subset(ces, election==1984&obj_class2=="Middle Class"))
+model_concordance_vote_upper_class<-update(model_concordance_vote_working_class, data=subset(ces, election==1984&obj_class2=="Upper Class"))
+model_concordance_vote_list<-list(model_concordance_vote_working_class,
+                                  model_concordance_vote_middle_class,
+                                  model_concordance_vote_upper_class)
+summary(model_concordance_vote_working_class)
+modelsummary(model_concordance_vote_list,
+             fmt=2, stars=T, gof_omit=gof_omit_vector)
 concordance_vote_list_material_84<-list(model1a_concordance_vote_84_roc, model1b_concordance_vote_84_roc, model1c_concordance_vote_84_roc, model1d_concordance_vote_84_roc)
 modelsummary(concordance_vote_list_material_84,
              stars=T,
